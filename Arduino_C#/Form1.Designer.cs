f50d9205d05483eae5855bc65ee59d0dfe957f10
Monserrat Garcia Esponda <monserrat.garcia84@unach.mx>
2024-09-30 19:46:37 -0600

    Agregar archivos de proyecto.

---
+++ Arduino_C#/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Arduino_C_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bencender = new System.Windows.Forms.Button();
+            this.Bapagar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Bencender
+            // 
+            this.Bencender.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Bencender.Location = new System.Drawing.Point(74, 57);
+            this.Bencender.Name = "Bencender";
+            this.Bencender.Size = new System.Drawing.Size(126, 65);
+            this.Bencender.TabIndex = 0;
+            this.Bencender.Text = "ENCENDER";
+            this.Bencender.UseVisualStyleBackColor = false;
+            this.Bencender.Click += new System.EventHandler(this.Bencender_Click);
+            // 
+            // Bapagar
+            // 
+            this.Bapagar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Bapagar.Location = new System.Drawing.Point(74, 179);
+            this.Bapagar.Name = "Bapagar";
+            this.Bapagar.Size = new System.Drawing.Size(126, 66);
+            this.Bapagar.TabIndex = 1;
+            this.Bapagar.Text = "APAGAR";
+            this.Bapagar.UseVisualStyleBackColor = false;
+            this.Bapagar.Click += new System.EventHandler(this.Bapagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(293, 319);
+            this.Controls.Add(this.Bapagar);
+            this.Controls.Add(this.Bencender);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CerrandoForm1);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Bencender;
+        private System.Windows.Forms.Button Bapagar;
+    }
+}
+
---
+++ Arduino_C#/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Arduino_C_
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort ArduinoPort { get; }
+        public Form1()
+        {
+            InitializeComponent();
+            //crear serial port
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM8"; // cehcar en el equipo
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.Open();
+
+            //vincular eventos
+            this.FormClosing += CerrandoForm1;
+            this.Bapagar.Click += Bapagar_Click;
+            this.Bencender.Click += Bencender_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bapagar_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+
+        private void Bencender_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+
+        private void CerrandoForm1(object sender, FormClosingEventArgs e)
+        {
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+    }
+}
